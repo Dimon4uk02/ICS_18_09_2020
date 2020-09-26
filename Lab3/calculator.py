9b0d7c7f7e27fe9ddfc0ac7d5bc8aeb3f965341a
vladsakun <vlad120403@gmail.com>
2020-09-26 09:22:46 +0300

    Lab3

---
+++ Lab3/calculator.py
@@ -0,0 +1,43 @@
+operators = ("+", "-", "*", "/", "=")
+result = int(input("Введите ЦЕЛОЕ число \n"))
+
+while True:
+
+    inputValue = input(
+        "Введите оператор  '+', '-', '*', '/' или '=', чтобы завершить \n")
+
+    if inputValue not in operators:
+
+        print("Не правильный оператор \n")
+
+    else:
+        if inputValue == "=":
+
+            print("Конец \n")
+            break
+
+        else:
+
+            inputNumber = input("Введите ЦЕЛОЕ число \n")
+
+            try:
+                inputNumber = int(inputNumber)
+            except:
+                print("Не корректное число \n")
+
+            if inputValue == '+':
+                result += inputNumber
+
+            elif inputValue == '-':
+                result -= inputNumber
+
+            elif inputValue == '*':
+                result *= inputNumber
+
+            elif inputValue == '/':
+                result /= inputNumber
+
+            else:
+                print("Не правильный оператор \n")
+
+print("Результат " + str(result))
