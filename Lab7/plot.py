c5a1c8d9d24fc6c7354f1153dd1d9e51674fcb2b
vladsakun <vlad120403@gmail.com>
2020-11-23 10:58:44 +0200

    Finish lab 7

---
+++ Lab7/plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import *
+
+
+def f(t):
+    return 5 * np.cos(10 * t) * np.sin(3 * t) / exp(t ** (1 / 2))
+
+
+t = np.arange(0, 5, 0.01)
+y = f(t)
+
+plt.plot(t, y, 'm')
+
+plt.xlabel('t')
+plt.ylabel('y')
+plt.title('Y(x)=5*cos(10*x)*sin(3*x)/(x^(1/2)), x=[0...5]')
+
+plt.show()
